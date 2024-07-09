20c8aff6a37ac2aca506d727281921160bc84ba3
JULIO OKUDA <julio.okuda@gmail.com>
2024-07-09 05:55:54 +0000

    add intermediate files

--- models/staging/user.sql
+++ models/staging/user.sql
@@ -1,16 +1,12 @@
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
-with src_user as
-(
-    select *
-    from workshop_moderno_raw.public.user
-)
-select 
-    pu.id, 
-    pu.user_id, 
-    pu.username, 
-    pu.gender, 
-    pu.employment, 
-    pu.credit_card, 
+with src_user as (select * from workshop_moderno_raw.public.user)
+select
+    pu.id,
+    pu.user_id,
+    pu.username,
+    pu.gender,
+    pu.employment,
+    pu.credit_card,
     pu.subscription
 from src_user as pu
